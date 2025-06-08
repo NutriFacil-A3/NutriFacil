777af91a994a928a08bb6d60b6bb1768f2a2b604
GabrielV21 <gabrielvasconcelosv21@gmail.com>
2025-06-08 17:31:42 -0300

    feat: Criando classe de FileDB

--- src/main/java/org/nutrifacil/Main.java
+++ src/main/java/org/nutrifacil/Main.java
@@ -1,7 +1,10 @@
 package org.nutrifacil;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
